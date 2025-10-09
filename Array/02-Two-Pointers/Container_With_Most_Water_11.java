86d580ea9fe8c20ea25cc34db11a236770d7b399
Siddique-7 <muhammadsiddik687@gmail.com>
2025-10-09 15:17:54 +0530

    chore: rename folders for better structure

---
+++ Array/02-Two-Pointers/Container_With_Most_Water_11.java
@@ -0,0 +1,32 @@
+
+public class Container_With_Most_Water_11 {
+  /**
+     * Finds the maximum area that can be formed between two lines.
+     */
+    
+    public static int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int width = right - left;
+            int currentHeight = Math.min(height[left], height[right]);
+            int area = width * currentHeight;
+            maxArea = Math.max(maxArea, area);
+
+            // Move the pointer pointing to the shorter line
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int result = maxArea(height);
+        System.out.println("Maximum water container area: " + result);
+    }
+}
